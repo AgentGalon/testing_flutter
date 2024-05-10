848cc2d52440a4a40ed67eaf1b8bc39c3e9f1436
AgentGalon <zahranrafif911@gmail.com>
2024-05-11 01:00:29 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,83 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home: const AudioPlayer(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final player = AudioPlayer();
+
+  String formatDuration(Duration d) {
+    final minutes = d.inMinutes.remainder(60);
+    final seconds = d.inSeconds.remainder(60);
+    return "${minutes.toString().padLeft(2, "0")}:${seconds.toString().padLeft(2, "0")}";
+  }
+
+  void handlePlayPause() {
+    if (player.playing) {
+      player.pause();
+    } else {
+      player.play();
+    }
+  }
+
+  void handleSeek(double value) {
+    player.seek(Duration(seconds: value.toInt()));
+  }
+
+  Duration position = Duration.zero;
+  Duration duration = Duration.zero;
+
+  @override
+  void initState() {
+    super.initState();
+    player.setAsset("assets/audio/sepuh.mp3");
+
+    player.positionStream.listen((p) {
+      setState(() => position = p);
+    });
+
+    player.durationStream.listen((d) {
+      setState(() => duration = d!);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Audio Player"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(formatDuration(position)),
+            Slider(
+              min: 0,
+              max: duration.inSeconds.toDouble(),
+              value: position.inSeconds.toDouble(),
+              onChanged: handleSeek,
+            ),
+            Text(formatDuration(duration)),
+            IconButton(
+              icon: Icon(player.playing ? Icons.pause : Icons.play_arrow),
+              onPressed: handlePlayPause,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
